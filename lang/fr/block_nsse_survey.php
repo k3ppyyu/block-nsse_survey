e26af70d9506a665dcfef2017115b103dc42b543
thibaud <thibaud@yorku.ca>
2026-02-12 21:11:33 -0500

    Added French language file

---
+++ lang/fr/block_nsse_survey.php
@@ -0,0 +1,59 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * NSSE Survey block strings - French
+ *
+ * @package    block_nsse_survey
+ * @copyright  None
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['blockname'] = 'Sondage NSSE';
+$string['pluginname'] = 'Sondage NSSE';
+
+$string['csvdata'] = 'Données CSV';
+$string['csvdata_help'] = 'Collez les données CSV';
+$string['csvdatadefault'] = 'STUDENTID,SURVEYLINK
+1234567890, https://examplesurveylink.com?id=1234567890';
+
+$string['blockmessage'] = 'Message du bloc Sondage NSSE';
+$string['blockmessage_help'] = 'Message à afficher dans le bloc.';
+$string['blockmessagedefault'] = '
+<div>
+    <div>À l\'Institut de technologie Rose-Hulman, nous voulons que nos étudiants vivent la meilleure expérience éducative possible. L\'Enquête nationale sur l\'engagement des étudiants (NSSE) nous aide en fournissant des informations essentielles sur les activités à l\'intérieur et à l\'extérieur de la salle de classe. Votre participation est essentielle, j\'espère donc que vous répondrez à la NSSE&mdash;une opportunité réservée uniquement aux étudiants de première année et aux finissants.<br><br></div>
+    <div>En reconnaissance de votre participation, les étudiants qui complètent le sondage seront automatiquement inscrits dans un tirage aléatoire pour gagner l\'un des 3 articles de marque Rose-Hulman de la librairie (d\'une valeur jusqu\'à 150 $). Vos chances de gagner dépendent du nombre d\'étudiants qui complètent le sondage ; 1045 étudiants ont été invités à participer.<br><br></div>
+    <div>Pour obtenir de l\'aide pour vous connecter au sondage ou pour trouver les réponses à diverses questions, veuillez consulter : <a href="https://nssesurvey.org/contact.cfm">https://nssesurvey.org/contact.cfm</a>. Plus d\'informations sur la NSSE sont disponibles sur nsse.indiana.edu. Si vous avez d\'autres questions, veuillez me contacter à lovellmd@rose-hulman.edu ou 812-877-8318.<br><br></div>
+    <div>Merci d\'avance de prendre quelques moments pour réfléchir à vos expériences et nous aider à renforcer l\'Institut de technologie Rose-Hulman!<br><br></div>
+    <div>Cordialement,</div>
+    <div>Matt Lovell</div>
+    <div>Directeur principal</div>
+</div>';
+
+$string['matchfield'] = 'Champ utilisateur correspondant';
+$string['matchfield_help'] = 'Quel champ utilisateur sera utilisé pour correspondre au champ STUDENTID dans les données csv?';
+
+$string['placement'] = 'Placement du lien';
+$string['placement_help'] = 'Collez les données CSV';
+
+$string['headerimage'] = 'Image d\'en-tête';
+$string['headerimage_help'] = 'Téléchargez un fichier image à afficher dans l\'en-tête du bloc Sondage NSSE';
+
+$string['imageurl'] = 'Lien URL de l\'image';
+$string['imageurl_help'] = 'Entrez une URL vers laquelle les utilisateurs seront dirigés lorsqu\'ils cliqueront sur l\'image d\'en-tête';
+
+$string['headerimagealt'] = 'En-tête du sondage NSSE';
+
